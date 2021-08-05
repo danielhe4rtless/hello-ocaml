b584a26ae51fde62d97bdcf172e6764084f60539
danielhe4rt <idanielreiss@hotmail.com>
2021-08-05 00:49:54 -0300

    acho que arrumei o camelo

--- fodase.ml
+++ fodase.ml
@@ -1,1 +1,6 @@
-dasuygdasudjasidastgdasyhudjas
+let rec quicksort = function
+    | [] -> []                                               
+    | x :: xs ->
+              let smaller = List.filter ((>) x) xs in        
+              let bigger = List.filter ((<) x) xs in         
+                quicksort smaller @ [x] @ quicksort bigger;;
